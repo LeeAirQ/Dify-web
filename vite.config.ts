d09c2a2aca40210d2dfc4d2dffc872a68a587907
liheng <77561271+LeeAirQ@users.noreply.github.com>
2025-05-19 15:49:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,18 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'url'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  // 根据当前工作目录中的 `mode` 加载 .env 文件
-  const env = loadEnv(mode, process.cwd())
-  
-  return {
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    server: {
-      open: true,
-      proxy: {
-        '/api': {
-          target: env.VITE_DIFY_API_BASE_URL || 'http://120.77.168.202/v1',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          secure: false,
-          ws: true
-        }
-      }
-    },
-    // 环境变量配置
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
-    }
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    // 移除代理配置，因为Vercel不支持
   }
 })
